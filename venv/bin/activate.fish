c38c3b8beea9cff235b353ef2135c0eef93200b9
João Vitor Prazeres da Silva <vitorbr1223@gmail.com>
2025-07-27 18:46:58 -0300

    nova tela fundo

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV '/mnt/hd/vitor/Área de trabalho/joao-vitor/grupo-1/venv'
+set -gx VIRTUAL_ENV '/mnt/hd/vitor/Área de trabalho/main/grupo-1/venv'
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
